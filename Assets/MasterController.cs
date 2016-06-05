22960ada00c059b10944caa97f96addad9e40494
alex-ong <alex.ong@unsw.edu.au>
2016-06-05 17:00:40 +1000

    Create originalCube from prefab.

--- Assets/MasterController.cs
+++ Assets/MasterController.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class MasterController : MonoBehaviour {
     public GameObject selectedCube;
-    
+    public GameObject sampleCube;
 	// Use this for initialization
 	void Start () {
-	
+	    GameObject go = CreateCube();
+        go.SetActive(true);
 	}
-	
+    
+	protected GameObject CreateCube() {
+        GameObject go = GameObject.Instantiate(this.sampleCube);
+        go.transform.SetParent(this.transform);
+        return go;
+    }
+   
 	// Update is called once per frame
 	void Update () {
 	
@@ -18,6 +25,8 @@ public class MasterController : MonoBehaviour {
         if (this.selectedCube != null) {
             return;
         }
-        //TODO: handle click.
+        this.selectedCube = ch.gameObject;
+        this.selectedCube.GetComponent<TrueAspectRatio>().enabled = true;
+        this.selectedCube.GetComponent<GetGrid>().enabled = true;
     }
 }
--- Assets/TrueAspectRatio.cs
+++ Assets/TrueAspectRatio.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TrueAspectRatio : MonoBehaviour {
+public class TrueAspectRatio : MonoBehaviour
+{
     public float blowupSize = 10.0f;
-	// Use this for initialization
+    // Use this for initialization
     
     private Vector2 startTextureScale;
     private Vector2 startTextureOffset;
-    
     private Vector3 startScale;
     private Vector3 targetScale;
-    
-    private Vector3 startPosition;    
-    
+    private Vector3 startPosition;
     public float animationTime = 3.0f;
     private float animTimer = 0.0f;
     
-    void Start () {
-	    Renderer r = this.gameObject.GetComponent<Renderer>();
+    void Start ()
+    {
+        Renderer r = this.gameObject.GetComponent<Renderer> ();
         Texture t = r.material.mainTexture;
-        float aspect = t.width/(float)t.height;
-        this.targetScale = new Vector3(blowupSize*aspect,blowupSize*(1.0f/aspect),1.0f);
+        if (t.width > t.height) {
+            this.targetScale = new Vector3 (blowupSize, blowupSize * t.height / t.width, 1.0f);    
+        } else {
+            this.targetScale = new Vector3 (blowupSize * t.width / t.height, blowupSize, 1.0f);    
+        }
+        
+        
         this.startScale = this.transform.localScale;
         
         this.startTextureScale = r.material.mainTextureScale;
@@ -28,18 +32,19 @@ public class TrueAspectRatio : MonoBehaviour {
         this.startPosition = this.transform.position;
     }
   
-	// Update is called once per frame
-	void Update () {
-	    animTimer += Time.deltaTime;
-        float perc = animTimer/animationTime;
-        perc = Mathf.Clamp01(perc);
+    // Update is called once per frame
+    void Update ()
+    {
+        animTimer += Time.deltaTime;
+        float perc = animTimer / animationTime;
+        perc = Mathf.Clamp01 (perc);
         
-        Renderer r = this.gameObject.GetComponent<Renderer>();
-        r.material.mainTextureScale = Vector2.Lerp(startTextureScale,Vector2.one,perc);
-        r.material.mainTextureOffset = Vector2.Lerp(startTextureOffset,Vector2.zero,perc);
+        Renderer r = this.gameObject.GetComponent<Renderer> ();
+        r.material.mainTextureScale = Vector2.Lerp (startTextureScale, Vector2.one, perc);
+        r.material.mainTextureOffset = Vector2.Lerp (startTextureOffset, Vector2.zero, perc);
         
-        this.transform.localScale = Vector3.Lerp(startScale,targetScale,perc);
-        this.transform.position = Vector3.Lerp(this.startPosition,Vector3.zero,perc);
+        this.transform.localScale = Vector3.Lerp (startScale, targetScale, perc);
+        this.transform.position = Vector3.Lerp (this.startPosition, Vector3.zero, perc);
     
-	}
+    }
 }
