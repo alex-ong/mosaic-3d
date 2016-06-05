050be6dfa5885175807fa41f0fda525ff2632fed
alex-ong <alex.ong@unsw.edu.au>
2016-06-05 17:00:13 +1000

    Corrected wrong uv offset.

--- Assets/LoadImage.cs
+++ Assets/LoadImage.cs
@@ -18,11 +18,11 @@ public class LoadImage : MonoBehaviour {
         float aspect = t.width/(float)t.height;            
         if (aspect > 1.0f) { //width > height
             renderer.material.mainTextureScale = new Vector2(1.0f/aspect,1.0f);
-            renderer.material.mainTextureOffset = new Vector2(0.5f - (0.5f*1.0f/aspect),1.0f);
+            renderer.material.mainTextureOffset = new Vector2(0.5f - (0.5f*1.0f/aspect),0.0f);
         } else {
             aspect = t.height / (float)t.width;
             renderer.material.mainTextureScale = new Vector2(1.0f,1.0f/aspect);
-            renderer.material.mainTextureOffset = new Vector2(1.0f,0.5f - (0.5f*1.0f/aspect));
+            renderer.material.mainTextureOffset = new Vector2(0.0f,(0.5f - (0.5f*1.0f/aspect)));
         }
     }
     
