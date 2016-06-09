adcfd414821583a9f48e9ee67958f4ee76ecc5b4
alex-ong <alex.ong@unsw.edu.au>
2016-06-09 19:01:14 +1000

    Added DataLoader

---
+++ Assets/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataLoader : MonoBehaviour {
+    public string fileToLoad;
+	// Use this for initialization
+	void Start () {
+        WWW request = new WWW(fileToLoad);
+        
+        while(!request.isDone) {            
+        }
+        Debug.Log (request.error);
+        Debug.Log("Data: " + request.text);
+	}
+	
+}
